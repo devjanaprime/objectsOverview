c14280390c6d2b35bf0f3682501451818f166f28
devjanaprime <dev@primeacademy.io>
2016-11-17 14:07:31 -0600

    first

---
+++ readme.md
@@ -0,0 +1,4 @@
+Objects Overview
+================
+
+From Tau Live Chat 11-17-2016
---
+++ scripts/objects.js
@@ -0,0 +1,23 @@
+var ep4 = {
+  title: "Star Wars: A New Hope",
+  releaseYear: 1977,
+  director: "George Lucas"
+}
+
+var ep5 = {
+  title: "The Empire Strikes Back",
+  releaseYear: 1980,
+  director: "Irvin Kershner"
+}
+
+var ep6 = {
+  title: "Return of the Jedi",
+  releaseYear: 1983,
+  director: "Richard Marquand"
+}
+
+var ep7 = {
+  title: "The Force Awakens",
+  releaseYear: 2015,
+  director: "J.J. Abrams"
+}
